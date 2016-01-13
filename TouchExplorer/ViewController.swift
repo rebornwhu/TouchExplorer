56e9a67026093f09cec28185356e0529b61aa7dc
rebornwhu <rebornwhu@gmail.com>
2016-01-12 20:41:23 -0800

    Save progress for UI

--- TouchExplorer/ViewController.swift
+++ TouchExplorer/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var messageLabel: UILabel!
+    @IBOutlet var tapsLabel: UILabel!
+    @IBOutlet var touchesLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
